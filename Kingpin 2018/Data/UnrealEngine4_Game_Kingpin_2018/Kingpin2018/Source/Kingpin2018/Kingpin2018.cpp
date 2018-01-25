0b1e6f77f8c676b7cb2ad462ec9dab27a8a50cfb
LarissaKneefel <jade_ketan@hotmail.com>
2018-01-25 15:34:19 +0100

    undone

--- Kingpin 2018/Data/UnrealEngine4_Game_Kingpin_2018/Kingpin2018/Source/Kingpin2018/Kingpin2018.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-#include "Kingpin2018.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Kingpin2018, "Kingpin2018" );
- 
